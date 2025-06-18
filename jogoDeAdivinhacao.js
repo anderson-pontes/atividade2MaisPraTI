afdf707ef178aefef3bd661471f9dddf7cf2b677
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2025-06-18 09:37:26 -0300

    Jogo de Adivinhação

---
+++ jogoDeAdivinhacao.js
@@ -0,0 +1,22 @@
+const readline = require('readline-sync'); // readline-sync permite entrada síncrona
+
+const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+let tentativas = 0;
+let acertou = false;
+
+while (!acertou) {
+  const resposta = readline.question('Adivinhe o numero entre 1 e 100: ');
+  const palpite = parseInt(resposta);
+  tentativas++;
+
+  if (isNaN(palpite)) {
+    console.log('Por favor, digite um número válido.');
+  } else if (palpite < numeroSecreto) {
+    console.log('Mais alto!');
+  } else if (palpite > numeroSecreto) {
+    console.log('Mais baixo!');
+  } else {
+    console.log(`Parabéns! Você acertou em ${tentativas} tentativa(s).`);
+    acertou = true;
+  }
+}
